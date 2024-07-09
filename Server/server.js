9feeef81fc29754523b4e34165bd4109c87b9863
ChriskenM <chriskenmutuma0727@gmail.com>
2024-07-09 15:21:03 +0300

    setting up the frontend home page

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["Usersone", "Userstwo", "Users3", "user4"] })
-})
+
 
 app.listen(5000, () => { console.log("Server is running on port 5000")})
